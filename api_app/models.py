b6d974956f4bc886a76affeb7a26b1c5b099a86c
Marty_276 <martybedoya10@gmail.com>
2023-11-13 11:48:58 -0500

    models changed

--- api_app/models.py
+++ api_app/models.py
@@ -2,30 +2,30 @@ from django.db import models
 from datetime import date
 
 class Applications(models.Model):
-    full_name = models.CharField(max_length= 254)
-    social_security_number = models.IntegerField()
-    mailing_address = models.CharField(max_length=254)
-    city = models.CharField(max_length=254)
-    state = models.CharField(max_length=2)
+
+    full_name = models.CharField(max_length = 254)
+    ssn = models.CharField(max_length = 254)
+    address = models.CharField(max_length = 254)
+    city = models.CharField(max_length = 254)
+    state = models.CharField(max_length = 254)
     zip_code = models.IntegerField()
-    phone_number = models.IntegerField()
-    alternative_phone = models.IntegerField()
-    age = models.IntegerField()
-    email = models.EmailField(max_length=254)
+    birth_date = models.DateField()
+    drivers_license_number = models.CharField(max_length = 254)
+    phone = models.CharField(max_length = 254)
+    alternative_phone = models.CharField(max_length = 254, default = "")
+    email_address = models.CharField(max_length = 254)
 
-    hours_available = models.JSONField()
-    job_time = models.CharField(max_length=254)
-    hours_weekly = models.IntegerField()
-    night_working = models.BooleanField()
-    begin_date = models.DateField()
-
+    days_unavailable = models.CharField(max_length = 254)
+    full_or_part_time = models.CharField(max_length = 254)
+    day_or_night_shift = models.CharField(max_length = 254)
+    hours_per_week = models.IntegerField()
+    date_available_to_begin = models.DateField()
+    
     have_been_employed_with_us = models.BooleanField()
-    us_citizen = models.BooleanField()
+    is_authorized_to_work_in_the_us = models.BooleanField()
     have_been_convicted = models.BooleanField()
-    convicted_explain = models.TextField
-    has_drivers_license = models.BooleanField()
-    drivers_license_number = models.CharField(max_length=254)
-    drivers_license_state = models.CharField(max_length=254)
+    convicted_explanation = models.TextField(max_length = 1500, default = "")
+    glazier_experience = models.TextField(max_length = 1500, default = "")
 
-    reviewed = models.BooleanField(default=False)
+    reviewed = models.BooleanField(default = False)
     date_sent = models.DateField(default = date.today)
